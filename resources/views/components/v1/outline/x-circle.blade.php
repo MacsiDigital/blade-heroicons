44c4b63346218c67ba11735ce478b835ef9fc8a1
Colin Hall <colin@macsi.co.uk>
2024-12-20 01:57:04 +0000

    Create x-circle.blade.php

---
+++ resources/views/components/v1/outline/x-circle.blade.php
@@ -0,0 +1,4 @@
+@props(['strokeWidth' => 2, 'viewBox' => '0 0 24 24', 'fill' => 'none', 'stroke' => 'currentColor'])
+<x-heroicons::svg {{ $attributes }} :viewBox="$viewBox" :fill="$fill" :stroke="$stroke" :stroke-width="$strokeWidth" x-description="Heroicon name: outline/x-circle">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+</x-heroicons::svg>
